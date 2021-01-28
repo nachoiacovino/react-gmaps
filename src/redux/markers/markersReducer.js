bcdbb85835469b60cf1f2b282d1e01882d4f63a0
Nacho Iacovino <nacho.iacovino@gmail.com>
2021-01-28 02:59:09 +0100

    Can't add the same marker twice

--- src/redux/markers/markersReducer.js
+++ src/redux/markers/markersReducer.js
@@ -5,6 +5,10 @@ const initialState = [];
 export default (state = initialState, { type, payload }) => {
   switch (type) {
     case ADD_MARKER:
+      const markedToAdd = state.find(
+        (marker) => marker.lat === payload.lat && marker.lng === payload.lng,
+      );
+      if (markedToAdd) return state;
       return [...state, { ...payload }];
     default:
       return state;
